eb3c3a45dc1f182ee014bb1b07b1c0760613cc90
Limberg Del Valle <limbergdelvalle@gmail.com>
2024-02-15 14:23:44 -0600

    nuevos archivos de c++ que se me ovidaron

---
+++ sumaNumeros.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void sumaNumeros(){
+	int num1, num2;
+	cout<<"Ingrese el primer numero: "<<endl;
+	cin>>num1;
+	
+	cout<<"Ingrese el segundo numero: "<<endl;
+	cin>>num2;
+	
+	
+	cout<<"La suma de los numeros es: "<<num1+num2;
+	
+}
+
+int main(){
+	sumaNumeros();
+}
